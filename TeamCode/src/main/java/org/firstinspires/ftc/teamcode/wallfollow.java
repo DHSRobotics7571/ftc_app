4aebf6dc4d972ea39f09007dd46a95c4c9eb075f
DHSRobotics7571 <irenicn6969@gmail.com>
2016-12-19 08:56:04 -0500

    Create wallfollow.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wallfollow.java
@@ -0,0 +1,15 @@
+@TeleOp()
+public class wallfollow extends OpMode{
+  @Override
+  public vod loop(){
+  
+  }
+  @Override
+  public void init(){
+  
+  }
+  @Override
+  public void stop(){
+  
+  }
+}
